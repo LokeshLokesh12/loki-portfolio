5bd71cb5fbe9a9c2339ae25acbffc6838bf123d5
lokeslokesh12 <lokesh272203@gmail.com>
2022-11-17 21:12:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// animation for i'm loki hover effect
+let intro = document.querySelector('.intro-container')
+// console.log(intro);
+ 
+ intro.addEventListener("mouseover",(event)=>{
+    let target = event.target
+    // console.log(event);
+
+    // console.log(target.tagName);
+    if(target.className != "intro-container" && target.tagName != "IMG"){
+        target.classList.add('rubberBand');
+        target.classList.add('animated');
+    
+        setTimeout(()=>{
+            target.classList.remove('rubberBand');
+            target.classList.remove('animated');
+        },700)      
+    }
+})
+
+
+
+
+
+
